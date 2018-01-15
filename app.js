4d875e40e4033183c1482d3fdeeb38f163f938bf
miwa_shuntaro <>
2018-01-15 14:28:36 +0900

    score,name登録

--- app.js
+++ app.js
@@ -5,20 +5,30 @@ const connection = require('./mysqlConnection');
 const PORT = process.env.PORT || 3000;
 
 app.get(`/`, (req, res) => {
+  // connection.query(query, function(err, rows) {
+  //   connection.query(query2, function(err, rows2) {
+  //     res.render('users', {
+  //       title: 'ランキング',
+  //       List: rows,
+  //       Rank: rows2
+  //     });
+  //   });
+  // });
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('chat message', (msg) => {
+  console.log('接続成功');
+  socket.on('chat message', (score,name) => {
     //繋がった時に行われる動作
-    console.log('message: ' + msg);
-    var query = 'INSERT INTO total_score (score, user_name) VALUES ("150", ' + '"' + msg + '")';
+    console.log('score: ' + score);
+    console.log('name: ' + name);
+    var query = 'INSERT INTO total_score (score, user_name) VALUES ("'+ score +'", ' + '"' + name + '")';
     connection.query(query, function(err, rows) {
       // res.redirect('/tsts');
       console.log("登録成功！！");
     });
-    io.emit('chat message', msg);
+    io.emit('chat message',score ,name);
   });
 });
 
